7eb8f6c4f1a1596cd950ce4afa826ee64d0b0a5f
Luís Fernando Fernandes <luisfernando.fernandes@hotmail.com>
2022-05-29 09:28:20 -0300

    Adicionar arquivos de projeto.

---
+++ WealthMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WealthMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
